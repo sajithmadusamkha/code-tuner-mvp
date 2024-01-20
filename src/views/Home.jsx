0282a1f01e8656b193d413fe35915581e7167d09
sajithmadusamkha <sajithmadusankha02@gmail.com>
2024-01-20 20:21:18 +0530

    content updated

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Form, Row } from "react-bootstrap";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 export default function Home() {
   return (
@@ -15,18 +15,46 @@ export default function Home() {
       </div>
       <Form className="mt-3">
         <Row className="m-0">
-        <Form.Group as={Col} className="mb-3 col-6" controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows={22} className="custom-textarea" placeholder="Start typing your code here..."/>
-        </Form.Group>
+          <Form.Group
+            as={Col}
+            className="mb-3 col-6"
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Control
+              as="textarea"
+              rows={22}
+              className="custom-textarea"
+              placeholder="Start typing your code here..."
+            />
+          </Form.Group>
 
-        <Form.Group as={Col} className="mb-3 col-6" controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows={18} className="custom-textarea"/>
-        </Form.Group>
+          <Form.Group
+            as={Col}
+            className="mb-3 col-6"
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Control as="textarea" rows={18} className="custom-textarea" />
+          </Form.Group>
 
-        <Form.Group as={Col} className="mb-3 col-6 position-absolute bottom-0 end-0" controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows={8} className="custom-textarea" placeholder="Error / Explain"/>
-        </Form.Group>
+          <Form.Group
+            as={Col}
+            className="mb-3 col-6 position-absolute bottom-0 end-0"
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Control
+              as="textarea"
+              rows={8}
+              className="custom-textarea"
+              placeholder="Error / Explain"
+            />
+          </Form.Group>
         </Row>
+        <Button
+          variant="success"
+          style={{ marginLeft: "0.7rem", width: "5rem" }}
+        >
+          Run
+        </Button>
       </Form>
     </div>
   );
